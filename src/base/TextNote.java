340c91301b2a83fbb503f0eaa7383d247e833c64
Tim Lam <fbfbfbtom@gmail.com>
2016-10-05 02:57:51 +0800

    lab5

--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -2,47 +2,59 @@ package base;
 
 import java.io.*;
 
-public class TextNote extends Note implements Serializable {
-    protected String content;
+public class TextNote extends Note implements Serializable
+{
+	protected String content;
 
-    public TextNote(String s) {
-        super(s);
-    }
+	public TextNote(String s)
+	{
+		super(s);
+	}
 
-    public TextNote(String title, String content) {
-        super(title);
-        this.content = content;
-    }
+	public TextNote(String title, String content)
+	{
+		super(title);
+		this.content = content;
+	}
 
-    public TextNote(File f) throws IOException {
-        super(f.getName());
-        this.content = getTextFromFile(f.getAbsolutePath());
-    }
+	public TextNote(File f) throws IOException, ClassNotFoundException
+	{
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+	}
 
-    private String getTextFromFile(String absolutePath) throws IOException {
-        String result = "";
-        // TODO
-        BufferedReader br = new BufferedReader(new FileReader(new File(absolutePath)));
-        result = br.readLine();
-        return result;
-    }
+	private String getTextFromFile(String absolutePath) throws IOException, ClassNotFoundException
+	{
+		String result = "";
+		// TODO
+		File file = new File(absolutePath);
+		FileInputStream fis = new FileInputStream(file);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		TextNote tn = (TextNote) ois.readObject();
+		ois.close();
+		return tn.content;
+	}
 
-    public void exportTextToFile(String pathFolder) {
-        //TODO
-    	if (pathFolder.isEmpty())
-    		pathFolder=".";
-        File file = new File(pathFolder + File.separator + super.getTitle().replaceAll(" ", "_") + ".txt");
-        // TODO
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(this);
-            oos.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public void exportTextToFile(String pathFolder)
+	{
+		// TODO
+		if (pathFolder.isEmpty())
+			pathFolder = ".";
+		File file = new File(pathFolder + File.separator + super.getTitle().replaceAll(" ", "_") + ".txt");
+		// TODO
+		FileOutputStream fos = null;
+		try
+		{
+			fos = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(this);
+			oos.close();
+		} catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }

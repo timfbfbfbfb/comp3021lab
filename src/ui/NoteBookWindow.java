6a5b9d8226d20d61e668e94888fd6e31bb7a0fa9
LAM Chak Tim <ctlamaf@WIN2K.CS.UST.HK>
2016-10-26 11:27:24 +0800

    lab8

--- src/ui/NoteBookWindow.java
+++ src/ui/NoteBookWindow.java
@@ -105,14 +105,18 @@ public class NoteBookWindow extends Application {
 			@Override
 			public void handle(ActionEvent event) {
 				// TODO Auto-generated method stub
+				System.out.println(currentNote);
 				if (currentFolder == "-----" || currentNote == "") {
 					Alert alert = new Alert(AlertType.WARNING);
 					alert.setTitle("Warning");
 					alert.setContentText("Please select a folder and a note");
 					alert.showAndWait();
 				} else {
-					Folder f = noteBook.getFolders().get(noteBook.getFolders().indexOf(new Folder(currentFolder)));
-					TextNote n = new TextNote(currentNote, textAreaNote.getText());
+					Folder f = noteBook.getFolders().get(
+							noteBook.getFolders().indexOf(
+									new Folder(currentFolder)));
+					TextNote n = new TextNote(currentNote, textAreaNote
+							.getText());
 					f.getNotes().remove(n);
 					f.getNotes().add(n);
 				}
@@ -130,7 +134,9 @@ public class NoteBookWindow extends Application {
 					alert.setContentText("Please select a folder and a note");
 					alert.showAndWait();
 				} else {
-					Folder f = noteBook.getFolders().get(noteBook.getFolders().indexOf(new Folder(currentFolder)));
+					Folder f = noteBook.getFolders().get(
+							noteBook.getFolders().indexOf(
+									new Folder(currentFolder)));
 					f.removeNotes(currentNote);
 					updateListView();
 
@@ -147,13 +153,15 @@ public class NoteBookWindow extends Application {
 		HBox hbox1 = new HBox();
 		hbox1.setSpacing(8);
 
-		ImageView saveImg = new ImageView(new Image(new File("save.png").toURI().toString()));
+		ImageView saveImg = new ImageView(new Image(new File("save.png")
+				.toURI().toString()));
 		saveImg.setFitHeight(18);
 		saveImg.setFitWidth(18);
 		hbox1.getChildren().add(saveImg);
 		hbox1.getChildren().add(saveNote);
 
-		ImageView delImg = new ImageView(new Image(new File("delete.png").toURI().toString()));
+		ImageView delImg = new ImageView(new Image(new File("delete.png")
+				.toURI().toString()));
 		delImg.setFitHeight(18);
 		delImg.setFitWidth(18);
 		hbox1.getChildren().add(delImg);
@@ -190,10 +198,11 @@ public class NoteBookWindow extends Application {
 			public void handle(ActionEvent arg0) {
 				// TODO Auto-generated method stub
 				FileChooser fileChooser = new FileChooser();
-				fileChooser.setTitle("Please Choose An File Which Contains a NoteBook Object!");
+				fileChooser
+						.setTitle("Please Choose An File Which Contains a NoteBook Object!");
 
-				FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Serialized Object File (*.ser)",
-						"*.ser");
+				FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter(
+						"Serialized Object File (*.ser)", "*.ser");
 				fileChooser.getExtensionFilters().add(filter);
 
 				File file = fileChooser.showOpenDialog(stage);
@@ -218,7 +227,8 @@ public class NoteBookWindow extends Application {
 				noteBook.save(fileName);
 				Alert alert = new Alert(AlertType.INFORMATION);
 				alert.setTitle("Successfully saved");
-				alert.setContentText("You file has been saved to file " + fileName);
+				alert.setContentText("You file has been saved to file "
+						+ fileName);
 				alert.showAndWait().ifPresent(rs -> {
 					if (rs == ButtonType.OK) {
 						System.out.println("Pressed OK.");
@@ -228,13 +238,16 @@ public class NoteBookWindow extends Application {
 		});
 
 		Label searchLabel = new Label("Search : ");
-		searchLabel.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
+		searchLabel
+				.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
 
 		TextField searchBox = new TextField("");
-		searchBox.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
+		searchBox
+				.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
 
 		Button searchBtn = new Button("Search");
-		searchBtn.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
+		searchBtn
+				.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
 		searchBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -250,7 +263,8 @@ public class NoteBookWindow extends Application {
 					}
 
 				if (folder != null) {
-					ArrayList<Note> searchResult = new ArrayList<Note>(folder.searchNotes(currentSearch));
+					ArrayList<Note> searchResult = new ArrayList<Note>(folder
+							.searchNotes(currentSearch));
 					titleslistView.getItems().clear();
 					for (Note n : searchResult)
 						titleslistView.getItems().add(n.getTitle());
@@ -260,7 +274,8 @@ public class NoteBookWindow extends Application {
 		});
 
 		Button clearSearchBtn = new Button("Clear Search");
-		clearSearchBtn.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
+		clearSearchBtn
+				.setStyle("-fx-font-size: 15px; -fx-font-family: Courier, Arial");
 		clearSearchBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -269,13 +284,13 @@ public class NoteBookWindow extends Application {
 				currentNote = "";
 				searchBox.setText("");
 				textAreaNote.setText("");
-				currentNote = "";
 				titleslistView.getSelectionModel().clearSelection();
 				updateListView();
 			}
 		});
 
-		hbox.getChildren().addAll(buttonLoad, buttonSave, searchLabel, searchBox, searchBtn, clearSearchBtn);
+		hbox.getChildren().addAll(buttonLoad, buttonSave, searchLabel,
+				searchBox, searchBtn, clearSearchBtn);
 
 		return hbox;
 	}
@@ -285,23 +300,25 @@ public class NoteBookWindow extends Application {
 		foldersComboBox.getItems().clear();
 		titleslistView.getItems().clear();
 		textAreaNote.setText("");
+		currentFolder = currentNote = "";
 
 		for (Folder fd : noteBook.getFolders())
 			foldersComboBox.getItems().add(fd.getName());
 
-		foldersComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
-			@Override
-			public void changed(ObservableValue ov, Object t, Object t1) {
-				if (t1 == null)
-					return;
-				currentFolder = t1.toString();
-				// this contains the name of the folder selected
-				// TODO update listview
+		foldersComboBox.getSelectionModel().selectedItemProperty()
+				.addListener(new ChangeListener<Object>() {
+					@Override
+					public void changed(ObservableValue ov, Object t, Object t1) {
+						if (t1 == null)
+							return;
+						currentFolder = t1.toString();
+						// this contains the name of the folder selected
+						// TODO update listview
 
-				updateListView();
+						updateListView();
 
-			}
-		});
+					}
+				});
 	}
 
 	/**
@@ -318,60 +335,62 @@ public class NoteBookWindow extends Application {
 		for (Folder fd : noteBook.getFolders())
 			foldersComboBox.getItems().add(fd.getName());
 
-		foldersComboBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
-			@Override
-			public void changed(ObservableValue ov, Object t, Object t1) {
-				if (t1 == null)
-					return;
-				currentFolder = t1.toString();
-				// this contains the name of the folder selected
-				// TODO update listview
+		foldersComboBox.getSelectionModel().selectedItemProperty()
+				.addListener(new ChangeListener<Object>() {
+					@Override
+					public void changed(ObservableValue ov, Object t, Object t1) {
+						if (t1 == null)
+							return;
+						currentFolder = t1.toString();
+						// this contains the name of the folder selected
+						// TODO update listview
 
-				updateListView();
+						updateListView();
 
-			}
+					}
 
-		});
+				});
 
 		foldersComboBox.setValue("-----");
 
 		titleslistView.setPrefHeight(100);
 
-		titleslistView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Object>() {
-			@Override
-			public void changed(ObservableValue ov, Object t, Object t1) {
-				if (t1 == null)
-					return;
-				String title = t1.toString();
-				currentNote = title;
-				// This is the selected title
-				// TODO load the content of the selected note in
-				// textAreNote
-				String content = "";
+		titleslistView.getSelectionModel().selectedItemProperty()
+				.addListener(new ChangeListener<Object>() {
+					@Override
+					public void changed(ObservableValue ov, Object t, Object t1) {
+						if (t1 == null)
+							return;
+						String title = t1.toString();
+						currentNote = title;
+						// This is the selected title
+						// TODO load the content of the selected note in
+						// textAreNote
+						String content = "";
 
-				Folder folder = null;
-				Note note = null;
+						Folder folder = null;
+						Note note = null;
 
-				for (Folder fd : noteBook.getFolders())
-					if (fd.getName().equals(currentFolder)) {
-						folder = fd;
-						break;
+						for (Folder fd : noteBook.getFolders())
+							if (fd.getName().equals(currentFolder)) {
+								folder = fd;
+								break;
+							}
+
+						if (folder != null)
+							for (Note n : folder.getNotes())
+								if (n.getTitle().equals(title)) {
+									note = n;
+									break;
+								}
+
+						if (note instanceof TextNote)
+							content = ((TextNote) note).getContent();
+
+						textAreaNote.setText(content);
+
 					}
-
-				if (folder != null)
-					for (Note n : folder.getNotes())
-						if (n.getTitle().equals(title)) {
-							note = n;
-							break;
-						}
-
-				if (note instanceof TextNote)
-					content = ((TextNote) note).getContent();
-
-				textAreaNote.setText(content);
-
-			}
-		});
+				});
 
 		Button addAFolder = new Button("Add a Folder");
 		addAFolder.setOnAction(new EventHandler<ActionEvent>() {
@@ -387,17 +406,21 @@ public class NoteBookWindow extends Application {
 				if (result.isPresent()) {
 					// TODO
 
-					if (result.get().isEmpty() || noteBook.getFolders().contains(new Folder(result.get()))) {
+					if (result.get().isEmpty()
+							|| noteBook.getFolders().contains(
+									new Folder(result.get()))) {
 						Alert alert = new Alert(AlertType.WARNING);
 						alert.setTitle("Warning");
 						if (result.get().isEmpty())
 							alert.setContentText("Please input a valid folder name");
 						else
-							alert.setContentText("You already have a folder named with " + result.get());
+							alert.setContentText("You already have a folder named with "
+									+ result.get());
 						alert.showAndWait();
 					} else {
 						noteBook.getFolders().add(new Folder(result.get()));
 						updateNoteBookInfo();
+						foldersComboBox.getSelectionModel().selectLast();
 					}
 				}
 			}
@@ -429,13 +452,22 @@ public class NoteBookWindow extends Application {
 							alert.setContentText("Please input a valid note name");
 							alert.showAndWait();
 						} else {
-							noteBook.createTextNote(currentFolder, result.get());
-							updateListView();
-							Alert alert = new Alert(AlertType.INFORMATION);
-							alert.setTitle("Successful!");
-							alert.setContentText(
-									"Insert " + result.get() + " to folder " + currentFolder + " successfully!");
-							alert.showAndWait();
+							if (!noteBook.createTextNote(currentFolder,
+									result.get())) {
+								Alert alert = new Alert(AlertType.WARNING);
+								alert.setTitle("Warning");
+								alert.setContentText("You already have a note named with "
+										+ result.get());
+								alert.showAndWait();
+							} else {
+								updateListView();
+								Alert alert = new Alert(AlertType.INFORMATION);
+								alert.setTitle("Successful!");
+								alert.setContentText("Insert " + result.get()
+										+ " to folder " + currentFolder
+										+ " successfully!");
+								alert.showAndWait();
+							}
 						}
 					}
 				}
@@ -461,6 +493,7 @@ public class NoteBookWindow extends Application {
 
 		// TODO populate the list object with all the TextNote titles of the
 		// currentFolder
+		currentNote ="";
 		Folder current = null;
 		for (Folder fd : noteBook.getFolders())
 			if (fd.getName().equals(currentFolder)) {
@@ -472,7 +505,8 @@ public class NoteBookWindow extends Application {
 			for (Note n : current.getNotes())
 				list.add(n.getTitle());
 
-		ObservableList<String> combox2 = FXCollections.observableArrayList(list);
+		ObservableList<String> combox2 = FXCollections
+				.observableArrayList(list);
 		titleslistView.setItems(combox2);
 		textAreaNote.setText("");
 	}
@@ -503,21 +537,32 @@ public class NoteBookWindow extends Application {
 
 	private void loadNoteBook() {
 		NoteBook nb = new NoteBook();
-		nb.createTextNote("COMP3021", "COMP3021 syllabus", "Be able to implement object-oriented concepts in Java.");
-		nb.createTextNote("COMP3021", "course information",
+		nb.createTextNote("COMP3021", "COMP3021 syllabus",
+				"Be able to implement object-oriented concepts in Java.");
+		nb.createTextNote(
+				"COMP3021",
+				"course information",
 				"Introduction to Java Programming. Fundamentals include language syntax, object-oriented programming, inheritance, interface, polymorphism, exception handling, multithreading and lambdas.");
-		nb.createTextNote("COMP3021", "Lab requirement",
+		nb.createTextNote(
+				"COMP3021",
+				"Lab requirement",
 				"Each lab has 2 credits, 1 for attendence and the other is based the completeness of your lab.");
 
-		nb.createTextNote("Books", "The Throwback Special: A Novel",
-				"Here is the absorbing story of twenty-two men who gather every fall to painstakingly reenact what ESPN called “the most shocking play in NFL history” and the Washington Redskins dubbed the “Throwback Special”: the November 1985 play in which the Redskins’ Joe Theismann had his leg horribly broken by Lawrence Taylor of the New York Giants live on Monday Night Football. With wit and great empathy, Chris Bachelder introduces us to Charles, a psychologist whose expertise is in high demand; George, a garrulous public librarian; Fat Michael, envied and despised by the others for being exquisitely fit; Jeff, a recently divorced man who has become a theorist of marriage; and many more. Over the course of a weekend, the men reveal their secret hopes, fears, and passions as they choose roles, spend a long night of the soul preparing for the play, and finally enact their bizarre ritual for what may be the last time. Along the way, mishaps, misunderstandings, and grievances pile up, and the comforting traditions holding the group together threaten to give way. The Throwback Special is a moving and comic tale filled with pitch-perfect observations about manhood, marriage, middle age, and the rituals we all enact as part of being alive.");
-		nb.createTextNote("Books", "Another Brooklyn: A Novel",
-				"The acclaimed New York Times bestselling and National Book Award–winning author of Brown Girl Dreaming delivers her first adult novel in twenty years. Running into a long-ago friend sets memory from the 1970s in motion for August, transporting her to a time and a place where friendship was everything—until it wasn’t. For August and her girls, sharing confidences as they ambled through neighborhood streets, Brooklyn was a place where they believed that they were beautiful, talented, brilliant—a part of a future that belonged to them. But beneath the hopeful veneer, there was another Brooklyn, a dangerous place where grown men reached for innocent girls in dark hallways, where ghosts haunted the night, where mothers disappeared. A world where madness was just a sunset away and fathers found hope in religion. Like Louise Meriwether’s Daddy Was a Number Runner and Dorothy Allison’s Bastard Out of Carolina, Jacqueline Woodson’s Another Brooklyn heartbreakingly illuminates the formative time when childhood gives way to adulthood—the promise and peril of growing up—and exquisitely renders a powerful, indelible, and fleeting friendship that united four young lives.");
+		nb.createTextNote(
+				"Books",
+				"The Throwback Special: A Novel",
+				"Here is the absorbing story of twenty-two men who gather every fall to painstakingly reenact what ESPN called â€œthe most shocking play in NFL historyâ€� and the Washington Redskins dubbed the â€œThrowback Specialâ€�: the November 1985 play in which the Redskinsâ€™ Joe Theismann had his leg horribly broken by Lawrence Taylor of the New York Giants live on Monday Night Football. With wit and great empathy, Chris Bachelder introduces us to Charles, a psychologist whose expertise is in high demand; George, a garrulous public librarian; Fat Michael, envied and despised by the others for being exquisitely fit; Jeff, a recently divorced man who has become a theorist of marriage; and many more. Over the course of a weekend, the men reveal their secret hopes, fears, and passions as they choose roles, spend a long night of the soul preparing for the play, and finally enact their bizarre ritual for what may be the last time. Along the way, mishaps, misunderstandings, and grievances pile up, and the comforting traditions holding the group together threaten to give way. The Throwback Special is a moving and comic tale filled with pitch-perfect observations about manhood, marriage, middle age, and the rituals we all enact as part of being alive.");
+		nb.createTextNote(
+				"Books",
+				"Another Brooklyn: A Novel",
+				"The acclaimed New York Times bestselling and National Book Awardâ€“winning author of Brown Girl Dreaming delivers her first adult novel in twenty years. Running into a long-ago friend sets memory from the 1970s in motion for August, transporting her to a time and a place where friendship was everythingâ€”until it wasnâ€™t. For August and her girls, sharing confidences as they ambled through neighborhood streets, Brooklyn was a place where they believed that they were beautiful, talented, brilliantâ€”a part of a future that belonged to them. But beneath the hopeful veneer, there was another Brooklyn, a dangerous place where grown men reached for innocent girls in dark hallways, where ghosts haunted the night, where mothers disappeared. A world where madness was just a sunset away and fathers found hope in religion. Like Louise Meriwetherâ€™s Daddy Was a Number Runner and Dorothy Allisonâ€™s Bastard Out of Carolina, Jacqueline Woodsonâ€™s Another Brooklyn heartbreakingly illuminates the formative time when childhood gives way to adulthoodâ€”the promise and peril of growing upâ€”and exquisitely renders a powerful, indelible, and fleeting friendship that united four young lives.");
 
 		nb.createTextNote("Holiday", "Vietnam",
 				"What I should Bring? When I should go? Ask Romina if she wants to come");
-		nb.createTextNote("Holiday", "Los Angeles", "Peter said he wants to go next Agugust");
-		nb.createTextNote("Holiday", "Christmas", "Possible destinations : Home, New York or Rome");
+		nb.createTextNote("Holiday", "Los Angeles",
+				"Peter said he wants to go next Agugust");
+		nb.createTextNote("Holiday", "Christmas",
+				"Possible destinations : Home, New York or Rome");
 		noteBook = nb;
 
 	}
